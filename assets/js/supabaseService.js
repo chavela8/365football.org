69de319e94591274c131c514a78f6c2436f504e5
chavela8 <chaveka1989@gmail.com>
2025-02-19 17:52:30 +0500

    Добавлен .env и обновлён .gitignore для исключения env файлов, обновлена инициализация Supabase

--- assets/js/supabaseService.js
+++ assets/js/supabaseService.js
@@ -1,4 +1,13 @@
-import { supabase } from './supabaseClient.js';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  console.error('Supabase URL или ключ не определены. Проверьте настройки переменных окружения.');
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getFootballData() {
   const { data, error } = await supabase
